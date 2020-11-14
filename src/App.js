e61b606e6d04889d98538726687b9d0c7ca9e0e5
zouhour <zouhour.bahri@yahoo.com>
2020-11-14 21:26:14 +0100

    default props added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import './App.css';
 import Profile from "./profile/Profile"
 import photo from "./profile/image/photo.png"
 
-const props = {
-  fullName: "Bahri Zouhour",
-  formation: "Fullstack js",
-  profession: "Web developer",
-}
-
 function App() {
   const handleName = (fullName) => alert(fullName);
   return (
     <div className="App">
-      <Profile user={props} handleName={handleName}>
+      <Profile fullName="BAHRI Zouhour" formation= "full stack JS" profession="Web developer" handleName={handleName}>
       <img className="photo" src={photo} alt="" />
       </Profile>
     </div>
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,24 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 
-function Profile(props  ) {
+function Profile(props) {
     // const { user , handleName , children } = props 
     console.log(props)
+const styleObject={ display:'flex', alignItems: 'center', margin: '50px', justifyContent: 'space-around'}
 
     return (
         <div>
-            <div className="position">
-            <div className="text">
-            <h1> fullName : {props.user.fullName} </h1>
-            <h1> formation : {props.user.formation} </h1>
-            <h1> profession : {props.user.profession} </h1>
+            <div  style= {styleObject}>
+                <div className="text">
+                    <h1> fullName : {props.fullName} </h1>
+                    <h1> formation : {props.formation} </h1>
+                    <h1> profession : {props.profession} </h1>
+                </div>
+                {props.children}
             </div>
-             {props.children}
-            </div>
-            <button onClick={() => props.handleName(props.user.fullName)}> Click Me </button> 
-             
+            <button onClick={() => props.handleName(props.fullName)}> Click Me </button>
+
         </div>
     )
+};
+
+Profile.defaultProps = {
+    fullName: "you can call me flowers",
+    formation: "learning js",
+    profession: "learning web development",
 }
 
+Profile.propTypes = {
+    fullName: PropTypes.string,
+    formation: PropTypes.string,
+    profession: PropTypes.string,
+    handleName: PropTypes.func,
+};
 export default Profile;
